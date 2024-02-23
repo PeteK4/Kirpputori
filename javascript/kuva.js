ed118c136d329da60639f17ba4acb78a8d3bab37
Terttu Toivonen <taika63@gmail.com>
2024-02-23 08:38:16 +0200

    Tuotteiden kuvat.js ja css muokkaus, sekä html koodi

---
+++ javascript/kuva.js
@@ -0,0 +1,56 @@
+document.getElementById('product-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const productName = document.querySelector('.product-name').value;
+    const productDescription = document.querySelector('.product-description').value;
+    const productPrice = document.querySelector('.product-price').value;
+    const imageFile = document.querySelector('.image-upload').files[0];
+  
+    const productPreviews = document.getElementById('product-previews');
+  
+    const productPreview = document.createElement('div');
+    productPreview.classList.add('product-preview-item'); // Lisää luokka
+  
+    const imagePreview = document.createElement('img');
+    imagePreview.src = URL.createObjectURL(imageFile);
+    imagePreview.alt = 'Tuotteen kuva';
+    imagePreview.width = 200; // Aseta kuvan leveys pikseleinä
+  
+  
+    const productDetails = document.createElement('div');
+    productDetails.classList.add('product-details'); // Lisää luokka
+  
+    const productNameElement = document.createElement('h2');
+    productNameElement.textContent = productName;
+  
+  
+    const productDescriptionElement = document.createElement('p');
+    productDescriptionElement.textContent = productDescription;
+   
+  
+    const productPriceElement = document.createElement('p');
+    productPriceElement.textContent = 'Hinta:' + productPrice + ' €';
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Poista';
+    deleteButton.classList.add('delete-button');
+    deleteButton.type = 'button'; // Estää lähettämästä lomaketta
+  
+    deleteButton.addEventListener('click', function() {
+        // Tässä voit lisätä koodin tuotteen poistamiseksi
+        productPreview.remove(); // Poista koko tuotteen esikatselu
+    });
+  
+     // Lisää elementit productDetails-diviin
+     productDetails.appendChild(productNameElement);
+     productDetails.appendChild(productDescriptionElement);
+     productDetails.appendChild(productPriceElement);
+   
+     // Lisää kuva ja tiedot productPreview-diviin
+     productPreview.appendChild(imagePreview);
+     productPreview.appendChild(productDetails);
+     productPreview.appendChild(deleteButton); // Lisää poista-nappi
+   
+     // Lisää tuotteen esikatselu tuotepreview-containeriin
+     productPreviews.appendChild(productPreview);
+  });
