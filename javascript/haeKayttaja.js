92668b8907263ce49984e5f194220b1f8a46a641
PeteK4 <petek2012@outlook.com>
2024-03-07 14:30:14 +0200

    Muokattu

--- javascript/haeKayttaja.js
+++ javascript/haeKayttaja.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const nimi = JSON.parse(localStorage.getItem(localStorage.getItem("kayttajaTunnus"))).nimi;
-    document.getElementById("kayttajaNimi").innerText = nimi + "!";
+    const kayttaja = JSON.parse(localStorage.getItem(localStorage.getItem("kayttajaTunnus")));
+    const nimi = kayttaja.nimi;
+    const etunimi = nimi.split(" ")[0]; // Erotetaan etunimi välilyönnin kohdalta
+    document.getElementById("kayttajaNimi").innerText = etunimi + "!";
 });
