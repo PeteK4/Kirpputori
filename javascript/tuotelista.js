cc0df3b76dbe65ecaed1f4d27fbac3cefd5c58aa
PeteK4 <petek2012@outlook.com>
2024-03-21 13:47:28 +0200

    Muokattu

--- javascript/tuotelista.js
+++ javascript/tuotelista.js
@@ -120,6 +120,6 @@ function displayProducts(products) {
 }
 
 // Määritellään kuinka monta tuotetta näytetään per sivu
-const productsPerPage = 2;
+const productsPerPage = 10;
 let currentPage = 1;
 
