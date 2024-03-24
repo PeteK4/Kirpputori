d4318b8f0132045cfa7fc0a3bc6020352abf2a50
PeteK4 <petek2012@outlook.com>
2024-03-24 10:43:10 +0200

    Tehty linkkiNapit.html yläpalkin napeille, josta ne haetaan jokaiseen html dokumenttiin omalla funktiolla (napit.js)

--- javascript/napit.js
+++ javascript/napit.js
@@ -1,22 +1,38 @@
-document.getElementById("nappiEtusivu").addEventListener("click", function() {
-    window.location.href = "etusivu.html";
-});
+function lataaNapit(id) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(id).innerHTML = this.responseText;
+            seuraaNappeja();
+        }
+    };
+    xhttp.open("GET", "linkkiNapit.html", true);
+    xhttp.send();
+}
 
-document.getElementById("nappiKategoriat").addEventListener("click", function() {
-    window.location.href = "kategoriat.html";
-});
+function seuraaNappeja() {
+    document.getElementById("nappiEtusivu").addEventListener("click", function() {
+        window.location.href = "etusivu.html";
+    });
 
-document.getElementById("nappiIlmoita").addEventListener("click", function() {
-    window.location.href = "ilmoitaTuote.html";
-});
+    document.getElementById("nappiKategoriat").addEventListener("click", function() {
+        window.location.href = "kategoriat.html";
+    });
 
-document.getElementById("nappiTuotehaku").addEventListener("click", function() {
-    window.location.href = "tuotehaku.html";
-});
+    document.getElementById("nappiIlmoita").addEventListener("click", function() {
+        window.location.href = "ilmoitaTuote.html";
+    });
 
-document.getElementById("nappiKirjauduUlos").addEventListener("click", function() {
-    if (confirm("Vahvista uloskirjautuminen.")) {
-        localStorage.setItem("kirjautunut", "eiKirjautunut");
-        window.location.href = "index.html";
-    }
-});
+    document.getElementById("nappiTuotehaku").addEventListener("click", function() {
+        window.location.href = "tuotehaku.html";
+    });
+
+    document.getElementById("nappiKirjauduUlos").addEventListener("click", function() {
+        if (confirm("Vahvista uloskirjautuminen.")) {
+            localStorage.setItem("kirjautunut", "eiKirjautunut");
+            window.location.href = "index.html";
+        }
+    });
+}
+
+lataaNapit("linkkiNapit");
